b5cc43c472d3bc9ea264692a7f0c126502bee769
J4WEB <prakharsrivastava841@gmail.com>
2022-09-09 15:43:39 +0530

    added day2 concepts

---
+++ Day2/Day2.py
@@ -0,0 +1,29 @@
+#Data Types
+#For int we can't do print(len(2345)) #give out an error message
+#Strings
+#this extraction of char by char is called subscripting
+# print("Hello"[4])
+
+
+#Fact in python we could write 123456987 if we write it as 
+# 123,456,789 its interpreted by python as 123456789 instead
+
+
+#Float - nums that have decimal points 
+#Eg 3.146789 is
+
+#Boolean- True or False 
+
+#Type conversion 
+#we use type( ) function to get the type of data type 
+#class<int> also with type checking we can change data types 
+#called type casting
+
+# new_num_char= str(number_it_is)
+
+print(70+int("30"))
+
+print("70"+"100")
+
+
+print(70+float("10.2"))
