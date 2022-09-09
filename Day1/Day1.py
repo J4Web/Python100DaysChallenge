763b5586c198e943a6551345bca0b007c6359c86
J4WEB <prakharsrivastava841@gmail.com>
2022-09-09 15:16:03 +0530

    Day1-Complete

---
+++ Day1/Day1.py
@@ -0,0 +1,13 @@
+# #How i did
+# print("Welcome to the Band Name Generator! \nWhat's name of the city you grew up in? ")
+# place=input()
+# print("What's your pet's name? ")
+# petName=input()
+# print("Your band name could be" + place + " "+petName)
+
+
+#How it was done 
+print("Welcome to the band Name Generator!\n")
+place=input("What's the name of the city you grew up in?\n")
+petName=input("What's your pet's name?\n")
+print("Your band name could be" + place + " "+petName)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Python100DaysChallenge
 This is a repo that describes my 100 days of learning python journey and having fun while doing it!
+
+
+#Day1 - Cool BandName Generator
+
+Link : https://replit.com/@PrakharSrivast5/Day1-Band-Name-Generator?v=1
